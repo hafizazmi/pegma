8df5cd49e5187d1d9f4305687dca77d6b6c50f55
Irfan Radzi <rolodex888@gmail.com>
2016-05-12 03:35:48 +0800

    Fucking logic

--- api/classes/Store.php
+++ api/classes/Store.php
@@ -3,6 +3,15 @@
 require 'db.php';
 
 class Store {
+
+    private function filter($clean_array, $result_object) {
+        $result_object['rating'] = [[
+            'aspect' => $res['aspect'],
+            'rating' => $res['bintang_value']
+        ]];
+        array_push($clean_array, $result_object);
+    }
+
     public function getAll() {
         $response = ['status' => false];
 
@@ -18,42 +27,85 @@ class Store {
 
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        $cleaned_result = [];
+        if ($result) {
 
-        foreach ($result as $r_k => $res) {
-            if (!sizeof($cleaned_result)) {
+            $cleaned_result = [];
 
-                // New Data Strucutre
-                $res['rating'] = [[
-                    'aspect' => $res['aspect'],
-                    'rating' => $res['bintang_value']
-                ]];
+            // Pembersihan Data (Group rating)
+            foreach ($result as $r_k => $res) {
+                if (!sizeof($cleaned_result)) {
 
-                array_push($cleaned_result, $res);
+                    // New Data Strucutre
+                    $res['ratings'] = [[
+                        'aspect_id' => $res['bintang_id'],
+                        'aspect' => $res['aspect'],
+                        'rate' => $res['bintang_value']
+                    ]];
 
-            } else {
-                foreach ($cleaned_result as $cr_k => &$cleaned_res) {
-                    if ($cleaned_res['id'] === $res['id']) {
-                        $temp = [
-                            'aspect' => $res['aspect'],
-                            'rating' => $res['bintang_value']
-                        ];
-                        array_push($cleaned_res['rating'], $temp);
-                    } else {
-                        $res['rating'] = [[
-                            'aspect' => $res['aspect'],
-                            'rating' => $res['bintang_value']
-                        ]];
+                    array_push($cleaned_result, $res);
 
-                        array_push($cleaned_result, $res);                        
+                } else {
+                    foreach ($cleaned_result as $cr_k => &$cleaned_res) {
+                        if ($cleaned_res['id'] === $res['id']) {
+                            $temp = [
+                                'aspect_id' => $res['bintang_id'],
+                                'aspect' => $res['aspect'],
+                                'rate' => $res['bintang_value']
+                            ];
+                            array_push($cleaned_res['ratings'], $temp);
+                        } else {
+                            $res['ratings'] = [[
+                                'aspect_id' => $res['bintang_id'],
+                                'aspect' => $res['aspect'],
+                                'rate' => $res['bintang_value']
+                            ]];
+
+                            array_push($cleaned_result, $res);                        
+                        }
                     }
                 }
             }
-        }
 
-        // echo json_encode($cleaned_result); exit;
+            $temp_rate = [];
 
-        if($result) {
+            // Kiraan bintang dan total rating
+            foreach ($cleaned_result as $k => &$value) {
+                foreach ($value['ratings'] as $__k => &$cr_rating) {
+                    if (!sizeof($temp_rate)) {
+                        $t = [
+                            'aspect_id' => $cr_rating['aspect_id'],
+                            'rate' => $cr_rating['rate'],
+                            'total' => 1
+                        ];
+                        array_push($temp_rate, $t);
+                    } else {
+                        foreach ($temp_rate as $k => &$r_v) {
+                            if ($r_v['aspect_id'] === $cr_rating['aspect_id']) {
+                                $r_v['rate'] = $r_v['rate'] + $cr_rating['rate'];
+                                $r_v['total']++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Letak Value Bintang berdasarkan ID
+            $exist = false;
+            foreach ($cleaned_result as $k => &$value) {
+                foreach ($value['ratings'] as $__k => &$cr_rating) {
+                    foreach ($temp_rate as $key => $rate) {
+                        if ($exist === $cr_rating['aspect_id']) {
+                            array_splice($value['ratings'], $__k, 1);
+                        } else {
+                            if ($rate['aspect_id'] === $cr_rating['aspect_id']) {
+                                $cr_rating['rate'] = $rate['rate'] / $rate['total'];
+                                $exist = $rate['aspect_id'];
+                            }
+                        }
+                    }
+                }
+            }
+
             $response = [
                 'status' => true,
                 'data' => $cleaned_result
